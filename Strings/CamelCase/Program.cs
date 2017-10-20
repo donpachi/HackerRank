3eaeccaa41e1fa2d9db5394cedb540f0f09edcf6
Aaron Kim <aaron.th.kim@gmail.com>
2017-10-19 23:56:35 -0600

    Imported test files from hackerrank

---
+++ Strings/CamelCase/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamelCase
+{
+    class CamelCase
+    {
+        static int CountWords(string s)
+        {
+            char[] charArr = s.ToCharArray();
+            int wordCount = 0;
+            if (s.Length > 0)
+            {
+                wordCount++;
+            }
+            foreach (char c in charArr)
+            {
+                if (c >= 'A' && c <= 'Z')
+                {
+                    wordCount++;
+                }
+            }
+            return wordCount;
+        }
+        static void Main(String[] args)
+        {
+            string s = Console.ReadLine();
+            do
+            {
+                Console.WriteLine(CountWords(s));
+            }
+            while ((s = Console.ReadLine()) != "");
+        }
+    }
+}
---
+++ Strings/Strings/SuperReducedString.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperReducedString
+{
+    class SuperReducedString
+    {
+        static string Super_reduced_string(string s)
+        {
+            if (s == "")
+            {
+                return "Empty String";
+            }
+            if (s.Length == 1)
+            {
+                return s;
+            }
+            int sameLetters = 1;
+            int appendCount = 0;
+            var charArray = s.ToCharArray();
+            StringBuilder sb = new StringBuilder(s.Length);
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (charArray[i] == charArray[i + 1])
+                {
+                    sameLetters++;
+                }
+                else
+                {
+                    if (sameLetters % 2 == 1)
+                    {
+                        sb.Append(charArray[i]);
+                        appendCount++;
+                    }
+                    sameLetters = 1;
+                }
+
+                if (i + 1 >= s.Length - 1)
+                {
+                    if (charArray[i] == charArray[i + 1] && sameLetters % 2 == 1)
+                    {
+                        sb.Append(charArray[i]);
+                        appendCount++;
+                    }
+                    else if (charArray[i] != charArray[i + 1])
+                    {
+                        sb.Append(charArray[i + 1]);
+                        appendCount++;
+                    }
+                }
+            }
+            if (sb.ToString().Length == 0)
+            {
+                return "Empty String";
+            }
+            else if (appendCount == s.Length)
+            {
+                return s;
+            }
+            return Super_reduced_string(sb.ToString());
+        }
+
+        static void Main(String[] args)
+        {
+            string s = Console.ReadLine();
+            string result;
+            do
+            {
+                result = Super_reduced_string(s);
+                Console.WriteLine(result);
+                Console.WriteLine("Press enter key to quit ... ");
+            } while ((s = Console.ReadLine()) != "");
+        }
+    }
+}
