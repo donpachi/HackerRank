819d30a102e60b0fc0f8e12adbbb9cbc8231e4aa
Aaron Kim <aaron.th.kim@gmail.com>
2017-10-21 20:19:56 -0600

    Finished maximum stack challenge, started two char challenge

---
+++ Stacks/Maximum Stack/MaxStackSolution.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum_Stack
+{
+    class MaxStackSolution
+    {
+        class MaxStack
+        {
+            private Stack<int> _maxStack;
+            private Stack<int> _mainStack;
+
+            public MaxStack()
+            {
+                _maxStack = new Stack<int>();
+                _mainStack = new Stack<int>();
+            }
+            public void Push(int element)
+            {
+                _mainStack.Push(element);
+                if (_maxStack.Count == 0)
+                {
+                    _maxStack.Push(element);
+                }
+                else if (element < _maxStack.Peek())
+                {
+                    _maxStack.Push(_maxStack.Peek());
+                }
+                else
+                {
+                    _maxStack.Push(element);
+                }
+            }
+
+            public void Pop()
+            {
+                _mainStack.Pop();
+                _maxStack.Pop();
+            }
+
+            public int Max()
+            {
+                return _maxStack.Peek();
+            }
+        }
+
+        static void Main(String[] args)
+        {
+            int numQueries = Convert.ToInt32(Console.ReadLine());
+            MaxStack stack = new MaxStack();
+            for (int i = 0; i < numQueries; i++)
+            {
+                int[] query = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                if (query[0] == 1)
+                    stack.Push(query[1]);
+                else if (query[0] == 2)
+                    stack.Pop();
+                else if (query[0] == 3)
+                    Console.WriteLine(stack.Max());
+                else
+                    throw new ArgumentException("Unexpected query type.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Strings/Two Characters/Two_Characters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Characters
+{
+    class Two_Characters
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            do
+            {
+                int len = s.Length;
+
+            } while ((s = Console.ReadLine()) != "");
+        }
+
+        static void TwoChars(string str, int strlength)
+        {
+
+        }
+    }
+}
